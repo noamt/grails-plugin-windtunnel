095d056d756d0755bcc17c7ecf2a212e1a630542
noamt <noam@10ne.org>
2014-02-25 09:25:00 +0200

    * Adding application title to stdout * Print usage when no path script is provided

--- src/main/groovy/org/_10ne/grails/windtunnel/executor/Main.groovy
+++ src/main/groovy/org/_10ne/grails/windtunnel/executor/Main.groovy
@@ -11,9 +11,19 @@ import java.nio.file.Paths
 class Main {
 
     public static void main(String[] args) {
+        println 'Grails Plugin Windtunnel - A blackbox testing framework for Grails plugins\n'
+
         CliBuilder cliBuilder = new CliBuilder(usage: 'gpw <script>')
+        cliBuilder.setFooter('https://github.com/noamt/grails-plugin-windtunnel')
+
         OptionAccessor optionAccessor = cliBuilder.parse(args)
-        String scriptPath = optionAccessor.arguments().first()
+        List<String> providedArguments = optionAccessor.arguments()
+        if (providedArguments.empty) {
+            cliBuilder.usage()
+            System.exit(1)
+        }
+
+        String scriptPath = providedArguments.first()
 
         def planEvaluator = new FlightPlanScripts()
         def flightPlan = planEvaluator.evaluate(Paths.get(scriptPath).toFile())
