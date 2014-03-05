9a9a7962501a1d3cacb065a6aa47ac316e4a42ab
noamt <noam@10ne.org>
2014-03-05 15:18:01 +0200

    Back the GVM Grails Pilot with SGVM

--- src/main/groovy/org/_10ne/grails/windtunnel/pilot/GvmGrailsPilot.groovy
+++ src/main/groovy/org/_10ne/grails/windtunnel/pilot/GvmGrailsPilot.groovy
@@ -1,10 +1,9 @@
 package org._10ne.grails.windtunnel.pilot
 
 import com.google.inject.Inject
+import org._10ne.sgvm.Gvm
 
-import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
 
 /**
  * @author Noam Y. Tenne.
@@ -14,14 +13,7 @@ class GvmGrailsPilot extends BaseGrailsPilot {
     @Inject
     @Override
     void init() {
-        Path grailsInstallation = Paths.get(System.getProperty('user.home'), '.gvm', 'grails', plan.grailsVersion)
-        if (Files.notExists(grailsInstallation)) {
-            throw new Exception("Unable to find Grails installation at: $grailsInstallation")
-        }
-        if (!Files.isReadable(grailsInstallation)) {
-            throw new Exception("Unable to access Grails installation at: $grailsInstallation")
-        }
-
+        Path grailsInstallation = Gvm.use().grails(version: plan.grailsVersion)
         init(grailsInstallation)
     }
 }
