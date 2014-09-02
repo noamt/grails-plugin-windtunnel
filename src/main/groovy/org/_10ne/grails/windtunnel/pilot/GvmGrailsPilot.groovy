d77b73fc52ab0e0f5fe798a162b90a168f6ff587
noamt <noam@10ne.org>
2014-09-02 16:45:29 +0300

    Add logging to the GVM Grails pilot

--- src/main/groovy/org/_10ne/grails/windtunnel/pilot/GvmGrailsPilot.groovy
+++ src/main/groovy/org/_10ne/grails/windtunnel/pilot/GvmGrailsPilot.groovy
@@ -1,6 +1,7 @@
 package org._10ne.grails.windtunnel.pilot
 
 import com.google.inject.Inject
+import groovy.util.logging.Slf4j
 import org._10ne.sgvm.Gvm
 
 import java.nio.file.Path
@@ -8,11 +9,13 @@ import java.nio.file.Path
 /**
  * @author Noam Y. Tenne.
  */
+@Slf4j
 class GvmGrailsPilot extends BaseGrailsPilot {
 
     @Inject
     @Override
     void init() {
+        log.info "Using Grails version ${plan.grailsVersion} (via the GVM-SDK)"
         Path grailsInstallation = Gvm.use.grails(version: plan.grailsVersion, install: true)
         init(grailsInstallation)
     }
